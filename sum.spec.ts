d45b9b3ddbfd84b0db01d069661607c95ec8c109
Your Name <you@example.com>
2024-07-10 13:02:55 +0530

    lets start

---
+++ sum.spec.ts
@@ -0,0 +1,6 @@
+import {stringSum} from "./sum"
+describe("Check Sum",()=>{
+    test(("initial input"),()=>{
+        expect(0).toEqual(0)
+    })
+})
---
+++ sum.ts
@@ -0,0 +1,4 @@
+export function stringSum(inputStr : string ) : number{
+    let res : number = 0
+    return 0
+}
